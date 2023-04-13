563933c90a1c3c558cf0802e33e9961b56c45565
Rei <ntptensai@gmail.com>
2023-04-13 22:51:01 +0900

    Optimize code in UserInterface and redesign a little bit

--- src/gui/UserInterface.java
+++ src/gui/UserInterface.java
@@ -28,12 +28,12 @@ public class UserInterface extends JFrame {
 	JButton btnPhong = new JButton("Phòng");
 	JButton btnDonDat = new JButton("Đơn Đặt");
 	JButton btnHoaDon = new JButton("Hóa Đơn");
-	JButton btnDichVu = new JButton("Dịch Vụ");
 	JButton btnTiepTan = new JButton("Tiếp Tân");
 	JButton btnThongKe = new JButton("Thống Kê");
 	JButton btnDangXuat = new JButton("Đăng Xuất");
 	
 	ArrayList<JButton> functionalButton = new ArrayList<JButton>();
+	ArrayList<JButton> taskbarButton = new ArrayList<JButton>();
 	
 	//
 	public UserInterface() {
@@ -70,13 +70,11 @@ public class UserInterface extends JFrame {
 		pnlTaskbar.add(btnPhong);
 		pnlTaskbar.add(btnDonDat);
 		pnlTaskbar.add(btnHoaDon);
-		pnlTaskbar.add(btnDichVu);
 		pnlTaskbar.add(btnTiepTan);
 		pnlTaskbar.add(btnThongKe);
 		pnlTaskbar.add(new JPanel() {private static final long serialVersionUID = 1L;{setOpaque(false);}});
+		pnlTaskbar.add(new JPanel() {private static final long serialVersionUID = 1L;{setOpaque(false);}});
 		pnlTaskbar.add(btnDangXuat);
-		
-		
 		
 		//Task Panel
 		pnlTask = new JPanel();
@@ -101,7 +99,6 @@ public class UserInterface extends JFrame {
 	public void showUI(JPanel panel) {
 		pnlTask.removeAll();
 		pnlTask.revalidate();
-		pnlTask.repaint();
 		pnlTask.setLayout(new BorderLayout());
 		pnlTask.add(panel, BorderLayout.CENTER);
 		setDecorateButton();
@@ -109,7 +106,7 @@ public class UserInterface extends JFrame {
 	}
 	public void checkPanelInstance(JPanel panel) {
 		if(panel instanceof UI_Phong) {
-			btnPhong.setBackground(Color.decode("#08FF08"));
+			btnPhong.setBackground(Color.decode("#3333FF"));
 		}
 	}
 	public void surroundPanel(JPanel panel, int surroundWidth) {
@@ -117,45 +114,38 @@ public class UserInterface extends JFrame {
 		
 		panel.add(new JPanel() {
 			private static final long serialVersionUID = 1L;
-			{setPreferredSize(new Dimension(0, surroundWidth));}}, BorderLayout.NORTH);
+			{setPreferredSize(new Dimension(0, surroundWidth));setBackground(Color.decode("#333333"));}}, BorderLayout.NORTH);
 		panel.add(new JPanel() {
 			private static final long serialVersionUID = 1L;
-			{setPreferredSize(new Dimension(0, surroundWidth));}}, BorderLayout.SOUTH);
+			{setPreferredSize(new Dimension(0, surroundWidth));setBackground(Color.decode("#333333"));}}, BorderLayout.SOUTH);
 		panel.add(new JPanel() {
 			private static final long serialVersionUID = 1L;
-			{setPreferredSize(new Dimension(surroundWidth, 0));}}, BorderLayout.WEST);
+			{setPreferredSize(new Dimension(surroundWidth, 0));setBackground(Color.decode("#333333"));}}, BorderLayout.WEST);
 		panel.add(new JPanel() {
 			private static final long serialVersionUID = 1L;
-			{setPreferredSize(new Dimension(surroundWidth, 0));}}, BorderLayout.EAST);
+			{setPreferredSize(new Dimension(surroundWidth, 0));setBackground(Color.decode("#333333"));}}, BorderLayout.EAST);
 	}
 	public void setDecorateButton() {
-		setDecorateOneButton(btnPhong);
-		setDecorateOneButton(btnDonDat);
-		setDecorateOneButton(btnHoaDon);
-		setDecorateOneButton(btnDichVu);
-		setDecorateOneButton(btnTiepTan);
-		setDecorateOneButton(btnThongKe);
-		setDecorateOneButton(btnDangXuat);
-		for(JButton thisButton : functionalButton) {
-			thisButton.setForeground(Color.decode("#FFFFFF"));
-			thisButton.setBackground(Color.decode("#8080ff"));
-		}
+		for(JButton thisButton : functionalButton)
+			setDecorateOneButton(thisButton);
+		btnDangXuat.setForeground(Color.decode("#222222"));
 		btnDangXuat.setBackground(Color.decode("#FF8080"));
 	}
 	public void setDecorateOneButton(JButton button) {
+		button.setForeground(Color.decode("#222222"));
+		button.setBackground(Color.decode("#8080FF"));
 		button.setPreferredSize(new Dimension(0, 50));
 		button.setFont(new Font("Arial", Font.PLAIN, 18));
 		button.setFocusPainted(false);
 		button.setBorder(BorderFactory.createBevelBorder(
-				BevelBorder.RAISED, Color.white, Color.black));
+				BevelBorder.RAISED, Color.white, Color.black));	
 	}
 	public void addFunctionalButton() {
 		functionalButton.add(btnPhong);
 		functionalButton.add(btnDonDat);
 		functionalButton.add(btnHoaDon);
-		functionalButton.add(btnDichVu);
 		functionalButton.add(btnTiepTan);
 		functionalButton.add(btnThongKe);
-		
+		functionalButton.add(btnDangXuat);
 	}
 }
