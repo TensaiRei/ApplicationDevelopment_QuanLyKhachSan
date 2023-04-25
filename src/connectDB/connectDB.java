6d966480cb34b64c0ec5693775905cee2d8c4476
Ludethuong <107840100+Ludethuong@users.noreply.github.com>
2023-04-25 18:08:04 +0700

    Update connectDB.java

--- src/connectDB/connectDB.java
+++ src/connectDB/connectDB.java
@@ -1,5 +1,27 @@
 package connectDB;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class connectDB {
-
+	public static Connection con = null;
+	private static connectDB instance = new connectDB();	
+	public static connectDB getInstance() {
+		return instance;
+	}
+	public void connect() throws SQLException {				
+			String url = "jdbc:sqlserver://localhost:1433;databasename=HotelManagement";
+			String user = "sa";
+			String password = "123456789";
+			con = DriverManager.getConnection(url, user, password);		
+	}	
+	public void disconnect() {
+		if (con != null)
+			try {
+				con.close();
+			} catch (SQLException e) {	e.printStackTrace();			}
+	}
+	
+	public static Connection getConnection() {
+		return con;
+	}
 }
