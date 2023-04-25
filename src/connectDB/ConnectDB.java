11b5d0eb86f757568b2d614945d77ff3da5e4535
Ludethuong <107840100+Ludethuong@users.noreply.github.com>
2023-04-25 19:33:20 +0700

    Update ConnectDB.java

--- src/connectDB/ConnectDB.java
+++ src/connectDB/ConnectDB.java
@@ -1,27 +1,45 @@
 package connectDB;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class ConnectDB {
-	public static Connection con = null;
-	private static ConnectDB instance = new ConnectDB();	
-	public static ConnectDB getInstance() {
-		return instance;
-	}
-	public void connect() throws SQLException {				
-			String url = "jdbc:sqlserver://localhost:1433;databasename=HotelManagement";
-			String user = "sa";
-			String password = "123456789";
-			con = DriverManager.getConnection(url, user, password);		
-	}	
-	public void disconnect() {
-		if (con != null)
-			try {
-				con.close();
-			} catch (SQLException e) {	e.printStackTrace();			}
-	}
+	//Connect Attributes
+	private static ConnectDB instance = new ConnectDB();
+	private static Connection connect = null;
 	
-	public static Connection getConnection() {
-		return con;
+	//Constructor
+	public ConnectDB() {}
+	
+	//Get
+	public static ConnectDB getInstance() {return instance;}
+	public static Connection getConnection() {return connect;}
+	
+	//Methods
+	public void connectDatabase() {
+		try {
+			//Login by Windows Authentication
+				//String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelManagement;integratedSecurity=true;";
+			
+			//Login by SQL Login
+				String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelManagement";
+				String username = "sa";
+				String password = "123456789";
+				
+			connect = DriverManager.getConnection(url, username, password);
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void disconnectDatabase() {
+		if(connect != null) {
+			try {
+				connect.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
